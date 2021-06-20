72e81d9e001f2d2619dfa8cdebb7fab05bbdc16b
Limeslices <Limeslices@users.noreply.github.com>
2021-06-19 23:00:38 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,9 +1,24 @@
 // create web audio api context
 //<i class="fas fa-sync fa-spin"></i> Extracting Water...
-const button = document.querySelector('button'); 
+const button = document.querySelector('button.ld'); 
 const stay = document.querySelector('p.stay');
-var html = `<i class="fas fa-sync fa-spin"></i> Extracting Water...`;
+const navbutton = document.querySelector('button#navbutt');
+const bottom = document.querySelector('a#more');
+var html = `<i class="fas fa-sync fa-spin"></i> Ejecting Water...`;
+var bar = new ldBar(
+  '.ldBar', /* Element or Selector for target element */
+  {
+    "stroke": 'rgb(170,225,233)',
+    "stroke-width": 10,
+    "preset": "line",
+    "value": 0,
+    "duration": 33
+    
+   } /* check Reference for supported options */
+);
 
+const loadingb = document.querySelector('div.ldBar')
+loadingb.style.display = "none"
 
 function playanimation() {
   n=button.classList;
@@ -17,17 +32,29 @@ function playanimation() {
 function playNote() {
   button.disabled = true
   button.innerHTML = html;
- 
+  loadingb.style.display = 'block'
   // create Oscillator node
   var audio = new Audio('165.mp3');
   var ding = new Audio('ding.mp3');
   audio.play();
- 
+  bar.set(
+    100,     /* target value. */
+    true   /* enable animation. default is true */
+  );
   setTimeout(
     function() {
       ding.play();
       button.disabled = false
-      button.innerHTML = `Extract Water`;
-    }, 10 * 1000);
+      button.innerHTML = `Eject Water`;
+      
+    }, 30 * 1000);
 
+}
+
+function hider() {
+  if (navbutton.attr('aria-expanded') === "true") {
+    bottom.style.display = 'none';
+  } else {
+    bottom.style.display = 'block';
+  }
 }
